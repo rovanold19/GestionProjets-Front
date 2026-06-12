e7c6c4b1d425d8bfd6238cd0ad97e194fcdaf284
rovanold19 <rovanold.yogno@saintjeaningenieur.org>
2026-06-12 01:48:23 +0100

    warning taille encore

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,35 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  // --- Routes des projets ---
+  {
+    path: 'projets/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'projets/modifier/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'projets/modifier/:id/creer',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'projets/modifier/:id/:id',
+    renderMode: RenderMode.Server
+  },
+
+  // --- Routes des tâches ---
+  {
+    path: 'taches/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'taches/:id/modifier', // <-- Ajout de la route manquante ici
+    renderMode: RenderMode.Server
+  },
+
+  // --- Toutes les autres routes statiques ---
   {
     path: '**',
     renderMode: RenderMode.Prerender
